80249d66610e8fd34004e11cbfcc9515cda46e80
A VENKATA HAREESH YADAV <141990014+hareeshyadav57@users.noreply.github.com>
2025-04-06 09:38:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config(); // Load .env variables
 
 const app = express();
-const PORT = 5000;
+const PORT =  process.env.PORT || 5000;
 
 app.use(cors());
 
